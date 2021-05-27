af41c4c238d1d5fa32eb03c66c5ff42110d3678d
Daniele Silvestro <silvestro.daniele@gmail.com>
2021-05-27 21:33:06 +0200

    v.0.1.12

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="npBNN",
-    version="0.1.10",
+    version="0.1.12",
     author="Daniele Silvestro and Tobias Andermann",
     description="Bayesian neural networks using Numpy and Scipy",
     long_description=long_description,
