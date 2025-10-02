5c24fbf2607452307a5fde623d87c10d6e4a17a9
Daniele Silvestro <silvestro.daniele@gmail.com>
2025-10-02 09:36:29 +0200

    fixed likelihood functions and added custom MSE

--- np_bnn/BNN_lik.py
+++ np_bnn/BNN_lik.py
@@ -9,8 +9,8 @@ def poi_likelihood(prediction, # 2D array: inst x (prms)
                    lik_temp=1,
                    sig2=0
                    ):
-    poi_rate = np.exp(prediction)
-    lik =  np.sum(scipy.stats.poisson.logpmf(true_values, poi_rate))
+    poi_rate = np.exp(prediction[:,0])
+    lik =  np.sum(scipy.stats.poisson.logpmf(true_values[:,0], poi_rate))
     return lik
 
 def negbin_likelihood(prediction, # 2D array: inst x (prms)
@@ -20,9 +20,12 @@ def negbin_likelihood(prediction, # 2D array: inst x (prms)
                       lik_temp=1,
                       sig2=0
                       ):
-    n = np.exp(prediction[:, 0])
+    # mean
+    nb_mean = np.exp(prediction[:, 0])
     p = 1 / (1 + np.exp(-prediction[:, 1]))
-    lik =  np.sum(scipy.stats.nbinom.logpmf(true_values, n=n, p=p))
+    n = p * nb_mean / (1 - p)
+    # "n = np.exp(prediction[:, 0])"
+    lik =  np.sum(scipy.stats.nbinom.logpmf(true_values[:,0], n=n, p=p))
     return lik
 
 def gamma_likelihood(prediction, # 2D array: inst x (prms)
@@ -36,4 +39,16 @@ def gamma_likelihood(prediction, # 2D array: inst x (prms)
     b = np.exp(prediction[:, 1])
     lik =  np.sum(scipy.stats.gamma.logpdf(true_values, a, b))
     return lik
+
+
+def negbin_acc(y, lab):
+    acc = np.mean( (np.exp(y[: ,0]) - lab[:, 0])**2 )
+    return acc
+
+def poi_acc(y, lab):
+    acc = np.mean( (np.exp(y[:, 0]) - lab[:, 0])**2 )
+    return acc
+
+def gamma_acc(y, lab):
+    acc = np.mean( (np.exp(y[:,0]) - lab.flatten())**2 )
 
